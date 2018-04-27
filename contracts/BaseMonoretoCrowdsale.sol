4b57acaa6be15212a7c1837cd31ac0bee4ab13ce
Nikolai Krivenko <nkrivenko35@gmail.com>
2018-04-27 23:21:48 +0300

    [SCA-1] BaseMonoretoCrowdsale unsafe math fix

--- contracts/BaseMonoretoCrowdsale.sol
+++ contracts/BaseMonoretoCrowdsale.sol
@@ -1,15 +1,19 @@
-pragma solidity ^0.4.19;
+pragma solidity 0.4.19;
 
 import "zeppelin-solidity/contracts/crowdsale/distribution/RefundableCrowdsale.sol";
 import "zeppelin-solidity/contracts/crowdsale/validation/CappedCrowdsale.sol";
 import "zeppelin-solidity/contracts/crowdsale/emission/MintedCrowdsale.sol";
+import "zeppelin-solidity/contracts/math/SafeMath.sol";
 
 import "./MonoretoToken.sol";
+
 
 /**
  * @title Base contract for Monoreto PreICO and ICO
  */
 contract BaseMonoretoCrowdsale is CappedCrowdsale, RefundableCrowdsale, MintedCrowdsale {
+
+    using SafeMath for uint256;
 
     uint256 public usdEth;
     uint256 public usdMnr;
@@ -27,52 +31,54 @@ contract BaseMonoretoCrowdsale is CappedCrowdsale, RefundableCrowdsale, MintedCr
 
     function BaseMonoretoCrowdsale(uint256 _tokenTarget, uint256 _usdEth, uint256 _usdMnr) public
     {
-	require(_tokenTarget > 0);
-	require(_usdEth > 0);
-	require(_usdMnr > 0);
+        require(_tokenTarget > 0);
+        require(_usdEth > 0);
+        require(_usdMnr > 0);
 
-	tokenTarget = _tokenTarget;
-	usdEth = _usdEth;
-	usdMnr = _usdMnr;
+        tokenTarget = _tokenTarget;
+        usdEth = _usdEth;
+        usdMnr = _usdMnr;
 
-	deployer = msg.sender;
+        deployer = msg.sender;
     }
 
     event UsdEthRateChanged(address indexed changerAddress, uint256 UsdEthRate);
     event UsdMnrRateChanged(address indexed changerAddress, uint256 UsdMnrRate);
 
     function setUsdEth(uint256 _usdEth) external onlyOwner {
-	rate = _usdEth.mul(CENT_DECIMALS).div(usdMnr);
         usdEth = _usdEth;
         UsdEthRateChanged(msg.sender, _usdEth);
+
+        rate = _usdEth.mul(CENT_DECIMALS).div(usdMnr);
     }
 
     function setUsdMnr(uint256 _usdMnr) external onlyOwner {
-	rate = usdEth.mul(CENT_DECIMALS).div(_usdMnr);
         usdMnr = _usdMnr;
         UsdMnrRateChanged(msg.sender, _usdMnr);
-    }
 
-    // If amount of wei sent is less than the threshold, revert.
-    uint256 private constant THRESHOLD = 100 finney;
-
-    function _preValidatePurchase(address _beneficiary, uint256 _weiAmount) internal {
-	require(tokensPurchased + _getTokenAmount(_weiAmount) <= tokenTarget);
-	require(_weiAmount >= THRESHOLD);
-	super._preValidatePurchase(_beneficiary, _weiAmount);
-    }
-
-    function _getTokenAmount(uint256 _weiAmount) internal view returns (uint256) {
-	return _weiAmount.mul(usdEth).mul(CENT_DECIMALS).div(usdMnr);
-    }
-
-    function _deliverTokens(address _beneficiary, uint256 _tokenAmount) internal {
-	super._deliverTokens(_beneficiary, _tokenAmount);
-	tokensPurchased = tokensPurchased.add(_tokenAmount);
+        rate = usdEth.mul(CENT_DECIMALS).div(_usdMnr);
     }
 
     function hasClosed() public view returns (bool) {
-	return super.hasClosed() || capReached();
+        return super.hasClosed() || capReached();
+    }
+
+    // If amount of wei sent is less than the threshold, revert.
+    uint256 public constant ETHER_THRESHOLD = 100 finney;
+
+    function _preValidatePurchase(address _beneficiary, uint256 _weiAmount) internal {
+        require(tokensPurchased.add(_getTokenAmount(_weiAmount)) <= tokenTarget);
+        require(_weiAmount >= ETHER_THRESHOLD);
+        super._preValidatePurchase(_beneficiary, _weiAmount);
+    }
+
+    function _getTokenAmount(uint256 _weiAmount) internal view returns (uint256) {
+        return _weiAmount.mul(usdEth).mul(CENT_DECIMALS).div(usdMnr);
+    }
+
+    function _deliverTokens(address _beneficiary, uint256 _tokenAmount) internal {
+        tokensPurchased = tokensPurchased.add(_tokenAmount);
+        super._deliverTokens(_beneficiary, _tokenAmount);
     }
 
     /**
@@ -82,10 +88,10 @@ contract BaseMonoretoCrowdsale is CappedCrowdsale, RefundableCrowdsale, MintedCr
      * AFTER executing its own finalizing actions.
      */
     function finalization() internal {
-	super.finalization();
+        super.finalization();
 
-	MonoretoToken castToken = MonoretoToken(token);
-	castToken.transferOwnership(deployer);
+        MonoretoToken castToken = MonoretoToken(token);
+        castToken.transferOwnership(deployer);
     }
 
 }
